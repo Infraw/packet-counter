9634c57ea332afafa17271702ce6765f962db996
Emir Akdag <infraw.linux@proton.me>
2026-01-31 16:41:51 +0300

    Add project skeleton and dependency imports

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use nfq::{Queue, Verdict};
+use std::process::Command;
+
+fn main() {
+    // TO DO: Initialize vars
+    // TO DO: Write iptables rules for forwarding packages to NFQUEUE
+    // TO DO: Open Queue
+    // TO DO: Accept All packets with Verdict
+    // TO DO: Print the output with print!("");
+    // TO DO: Clear app related iptables rules
+}
